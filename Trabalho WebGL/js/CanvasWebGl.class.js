2681d14a3944dd6cd721aa08ae7c0fa61c6ae1b8
Rafael Ferreira <mail@rafaelferreira.pt>
2015-11-27 12:33:20 +0000

    webgl obj add

--- Trabalho WebGL/js/CanvasWebGl.class.js
+++ Trabalho WebGL/js/CanvasWebGl.class.js
@@ -9,6 +9,10 @@ function CanvasWebGl(puzzle){
 	// models
 	this.models = {};
 
+	// background
+	this.back = null;
+
+
 	// canvas
 	this.canvas = document.getElementById("webgl-id");
 
@@ -19,21 +23,11 @@ function CanvasWebGl(puzzle){
 	this.initWebGL();
 
 	for(var i=0; i<this.puzzle.length; i++){
-		var url = this.puzzle[i][0];
-
-		$.ajax({
-		  url: url,
-		  type: 'get',
-		  dataType: 'text',
-		  async: false,
-		  success: function(data) {
-		      result = data;
-		  }
-		});
-
-		this.parseFile(this.puzzle[i], result);
+		var result = parseTXTfile(this.puzzle[i][0]);
+		this.newModel(result, this.puzzle[i]);
 	}
 
+	this.initBackground();
 	this.drawScene();
 }
 
@@ -71,37 +65,22 @@ CanvasWebGl.prototype.drawScene = function(){
 	for(var model in this.models){
 		this.models[model].drawScene(this.sx, this.sy, this.sz);
 	}
+
+	this.back.drawScene(this.sx, this.sy, this.sz);
 };
 
-CanvasWebGl.prototype.parseFile =  function(puzzle, data){
-	// Entire file read as a string
-	// The tokens/values in the file
-	// Separation between values is 1 or mode whitespaces
-	var tokens = data.split(/\s\s*/);
-
-	// Array of values; each value is a string
-	var numVertices = parseInt(tokens[0]);
-
-	// For every vertex we have 6 floating point values
-	var i, j;
-	var aux = 1;
-	var newVertices = [];
-	var newColors = []
-
-	for(i = 0; i < numVertices; i++){
-		for(j = 0; j < 3; j++){
-			newVertices[3 * i + j] = parseFloat(tokens[aux++]);
-		}
-
-		for(j = 0; j < 3; j++){
-			newColors[3 * i + j] = parseFloat(tokens[aux++]);
-		}
+CanvasWebGl.prototype.initBackground = function(){
+	var result = parseOBJfile("modelos/table.obj");
+	var colors = [];
+	for(var i=0; i<result["vertices"].length; i++){
+		colors.push(0.5);
+		result["vertices"][i] = result["vertices"][i] * 0.05;
 	}
+	this.back = new Models(this.gl, [0, 0, 0], result["vertices"].slice(), colors);
+};
 
-	// Assigning to the current model
-	this.models[puzzle[1]] = new Models(this.gl, puzzle[2], newVertices.slice(), newColors.slice());
-
-	// this.resetGlobalValues();
+CanvasWebGl.prototype.newModel =  function(result, puzzle){
+	this.models[puzzle[1]] = new Models(this.gl, puzzle[2], result["vertices"].slice(), result["colors"].slice());
 };
 
 // WebGL Initialization
@@ -125,3 +104,83 @@ CanvasWebGl.prototype.initWebGL =  function(){
 		alert("Could not initialise WebGL, sorry! :-(");
 	}
 };
+
+function parseOBJfile(url){
+	var result = null;
+
+	$.ajax({
+	  url: url,
+	  type: 'get',
+	  dataType: 'text',
+	  async: false,
+	  success: function(data) {
+	      result = data;
+	  }
+	});
+
+	var lines = result.split('\n');
+
+	// The new vertices
+	var newVertices = [];
+
+	// The new normal vectors
+	var newNormals = [];
+
+	// Check every line and store
+	for(var line = 0; line < lines.length; line++){
+			var tokens = lines[line].split(/\s\s*/);
+			if(tokens[0] == "v"){
+				for(j = 1; j < 4; j++) {
+				newVertices.push(parseFloat(tokens[j]));
+				}
+			}
+
+			if(tokens[0] == "vn"){
+				for(j = 1; j < 4; j++){
+					newNormals.push(parseFloat(tokens[j]));
+				}
+			}
+	}
+
+	return {"vertices": newVertices.slice(), "normals": newNormals.slice()};
+}
+
+function parseTXTfile(url){
+	var result = null;
+
+	$.ajax({
+	  url: url,
+	  type: 'get',
+	  dataType: 'text',
+	  async: false,
+	  success: function(data) {
+	      result = data;
+	  }
+	});
+
+	// Entire file read as a string
+	// The tokens/values in the file
+	// Separation between values is 1 or mode whitespaces
+	var tokens = result.split(/\s\s*/);
+
+	// Array of values; each value is a string
+	var numVertices = parseInt(tokens[0]);
+
+	// For every vertex we have 6 floating point values
+	var i, j;
+	var aux = 1;
+	var newVertices = [];
+	var newColors = []
+
+	for(i = 0; i < numVertices; i++){
+		for(j = 0; j < 3; j++){
+			newVertices[3 * i + j] = parseFloat(tokens[aux++]);
+		}
+
+		for(j = 0; j < 3; j++){
+			newColors[3 * i + j] = parseFloat(tokens[aux++]);
+		}
+	}
+
+	return {"vertices": newVertices.slice(), "colors": newColors.slice()};
+}
